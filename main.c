c38e56ea0801c31f6ab245388d5c22dbe1877134
rgpacas8 <ie706818@iteso.mx>
2020-04-03 20:33:52 -0600

    el programa se queda atorado en linea 204 de rtos_I2C.c

--- main.c
+++ main.c
@@ -17,14 +17,29 @@
 #include "mahony.h"
 /* TODO: insert other definitions and declarations here. */
 #define SYSTEM_CLOCK (10500000U)
-#define BAUD_RATE (9600U)
+#define BAUD_RATE (115200U)
 #define PIN_SCL_PTB2 (2U)
-#define PIN_SDA_PTB3 (2U)
+#define PIN_SDA_PTB3 (3U)
 #define GPIO_MUX2 (2U)
 /*
  * I2C_0
  * 	PTB2 -SCL AZUL
  * 	PTB3 -SDA
+ * 	MODULO IMU
+ * 	1 - VCC 3.3v	15 - NC
+ * 	2 - NC			16 - R=2.2k
+ * 	3 - GND			17 - R=2.2k
+ * 	4 - GND			18 - NC
+ * 	5 - NC			19 - NC
+ * 	6 - NC			20 - NC
+ * 	7 - VCC			21 - NC
+ * 	8 - NC			22 - NC
+ * 	9 - NC			23 - NC
+ * 	10 - NC			24 - NC
+ * 	11 - NC			25 - NC
+ * 	12 - NC			26 - NC
+ * 	13 - NC			27 - NC
+ * 	14 - NC			28 - NC
  */
 const rtos_i2c_config_t g_rtos_i2c_config = {
 		BAUD_RATE, rtos_i2c_0, rtos_i2c_portB,
@@ -44,10 +59,11 @@ int main(void) {
 	  	/* Init FSL debug console. */
 	    BOARD_InitDebugConsole();
 	    uint8_t dato=0x0;
+	    //HEY
 	    PRINTF("Hello World\n");
 	    rtos_i2c_init(g_rtos_i2c_config);
 	   // rtos_i2c_transfer(rtos_i2c_0, &dato, 7, 0x69, 0x00, 7);
-	    rtos_i2c_receive(rtos_i2c_0, &dato, 7, 0x69, 0x00, 7);
+	    rtos_i2c_receive(rtos_i2c_0, &dato, 7, 0x00, 0x69, 7);
 
 
     while(1) {
