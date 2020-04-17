d8c3c63f8dd2f8a84754001e5950dfcde53e0989
David Flores Ruiz <ie717807@iteso.mx>
2020-04-16 20:17:30 -0500

    Truena en receive() se va a semihosthardfault

--- main.c
+++ main.c
@@ -30,8 +30,8 @@
 /* TODO: insert other definitions and declarations here. */
 #define SYSTEM_CLOCK (10500000U)
 #define BAUD_RATE (100000)	// 100000 from Chava help antes teniamos 9600
-#define PIN_SCL_PTC10 (10U)
-#define PIN_SDA_PTC11 (11U)
+#define PIN_SCL_PTE24 (24U)
+#define PIN_SDA_PTE25 (25U)
 
 /* The I2C address of the device is 0b 110_1000 (0x68) if the SDO pin is pulled to GND */
 #define BMI160_SLAVE_ADDR_7bits 0x68 // Default I2C 7-bit address of device if SDO = GND
@@ -52,10 +52,10 @@
  * 	PTB2 -SCL AZUL
  * 	PTB3 -SDA
  * 	MODULO IMU
- * 	1 - VCC 3.3v	15 - NC
- * 	2 - NC			16 - R=2.2k
+ * 	1 - VCC 3.3v	15 - GND
+ * 	2 - NC			16 - NC
  * 	3 - GND			17 - R=2.2k
- * 	4 - GND			18 - NC
+ * 	4 - GND			18 - R=2.2k
  * 	5 - NC			19 - NC
  * 	6 - NC			20 - NC
  * 	7 - VCC			21 - NC
@@ -69,11 +69,11 @@
  */
 const rtos_i2c_config_t g_rtos_i2c_config = {
 		BAUD_RATE, 		/**I2C Baud rate*/
-		rtos_i2c_1, 	/**I2C to use*/
-		rtos_i2c_portC,	/**Kinetis Port*/
-		PIN_SCL_PTC10,	/**Pin of Serial Clock*/
-		PIN_SDA_PTC11,	/**Pin of Serial Data*/
-		kPORT_MuxAlt2	/**Pin Configuration*/
+		rtos_i2c_0, 	/**I2C to use*/
+		rtos_i2c_portE,	/**Kinetis Port*/
+		PIN_SCL_PTE24,	/**Pin of Serial Clock*/
+		PIN_SDA_PTE25,	/**Pin of Serial Data*/
+		kPORT_MuxAlt5	/**Pin Configuration*/
 };
 
 /*
@@ -99,7 +99,7 @@ int main(void) {
 	    uint8_t dataToWrite = ACCEL_NORMAL_MODE | GYROS_NORMAL_MODE;	// Byte to write
 	    uint8_t dataRead = 0x00;	// Read byte
 
-	    rtos_i2c_receive(rtos_i2c_1, &dataRead,    1, BMI160_SLAVE_ADDR_7bits, BMI160_CHIP_ID, 1);
+	    rtos_i2c_receive(rtos_i2c_0, &dataRead,    1, BMI160_SLAVE_ADDR_7bits, BMI160_CHIP_ID, 1);
 
 		if (dataRead == BMI160_CHIP_ID_VAL) {
 			printf("\nAqui estoy! BMI160... como sensor IMU externo \n");
